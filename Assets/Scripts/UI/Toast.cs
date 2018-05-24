ea00f89856b44c0aa7107b575cd3967abf2213db
CreepyCreature <iverson_emin@yahoo.com>
2018-05-24 16:13:46 +0300

    Added in-game Tutorial Text.

---
+++ Assets/Scripts/UI/Toast.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+
+public class Toast : MonoBehaviour
+{
+    public float duration = 5.0f;
+    public float fadeTime = 1.0f;
+
+    private Text _text;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _text = GetComponent<Text>();
+
+        StartCoroutine(DoTheThing());
+	}
+
+    private IEnumerator DoTheThing()
+    {
+        float timeToLive = duration;
+        while (timeToLive > 0)
+        {
+            timeToLive -= Time.deltaTime;
+            yield return null;
+        }
+
+        _text.CrossFadeAlpha(0.0f, fadeTime, false);
+        Destroy(gameObject, fadeTime);
+    }
+}
