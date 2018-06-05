6e6ea0eddd0728e941f6927de4cee5d7c8db2985
CreepyCreature <iverson_emin@yahoo.com>
2018-06-05 21:22:27 +0300

    Tried reducing nausea from VOMIT MODE

--- Assets/Scripts/Camera/RotatingCam.cs
+++ Assets/Scripts/Camera/RotatingCam.cs
@@ -7,8 +7,34 @@ public class RotatingCam : MonoBehaviour
     [SerializeField]
     private Transform target;
 
+    public bool smoothOut = true;
+
+    public float smoothTime = 0.4f;
+    private Vector3 _velocity = Vector3.zero;
+
     private void LateUpdate()
     {
+        if (smoothOut)
+        {
+            NonVomitingRotation();
+        }
+        else
+        {
+            VomitingRotation();
+        }
+    }
+
+    private void VomitingRotation()
+    {
         transform.rotation = target.rotation;
+    }
+
+    private void NonVomitingRotation()
+    {
+        Vector3 targetRotation = new Vector3(
+            transform.localEulerAngles.x, transform.localEulerAngles.y, target.localEulerAngles.z);
+
+        transform.localEulerAngles = Vector3.SmoothDamp(transform.localEulerAngles,
+            targetRotation, ref _velocity, smoothTime);
     }
 }
