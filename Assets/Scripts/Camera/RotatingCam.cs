7ef585bc102b01837d61e1e3aff5dedfa76567db
CreepyCreature <iverson_emin@yahoo.com>
2018-06-05 21:16:30 +0300

    ACTIVATED VOMIT MODE

---
+++ Assets/Scripts/Camera/RotatingCam.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingCam : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    private void LateUpdate()
+    {
+        transform.rotation = target.rotation;
+    }
+}
